0e606ff5ff88cc628aad135534b89c28fdd63829
TheOnion313 <royn77777@gmail.com>
2021-08-07 22:22:58 +0300

    Roy - Documentation for Registration page.

--- app/src/main/java/com/example/scoutingtest4590/MainActivity2.java
+++ app/src/main/java/com/example/scoutingtest4590/MainActivity2.java
@@ -18,13 +18,13 @@ import com.example.scoutingtest4590.util.Net;
 import java.util.Random;
 
 public class MainActivity2 extends AppCompatActivity {
-    String POOL = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    int SIZE = 30;
+    String POOL = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"; // all possible letters for pass
+    int SIZE = 30; // pass size
     Random r = new Random();
 
     public String genPass() {
         String ret = "";
-        for (int i = 0; i < SIZE; i++) {
+        for (int i = 0; i < SIZE; i++) { // picks random char from POOL, SIZE times and appends to string.
             int idx = r.nextInt(POOL.length());
             ret = ret.concat(POOL.substring(idx, idx + 1));
         }
@@ -42,35 +42,35 @@ public class MainActivity2 extends AppCompatActivity {
         EditText PIN = (EditText) findViewById(R.id.ENTER_PIN);
         Button LOGIN = findViewById(R.id.LOGIN);
 
-        SharedPreferences sharedPref = MainActivity2.this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences sharedPref = MainActivity2.this.getPreferences(Context.MODE_PRIVATE); // access phone memory
         @SuppressLint("CommitPrefEdits") SharedPreferences.Editor editor = sharedPref.edit();
-        if (sharedPref.getString("password", "0").equals("0")) {
-            editor.putString("password", genPass());
+        if (sharedPref.getString("password", "0").equals("0")) { // if didn't save anything for password must generate one and save it
+            editor.putString("password", genPass()); // save generated password
             editor.apply();
         }
 
 
-        LOGIN.setOnClickListener(v -> {
+        LOGIN.setOnClickListener(v -> { // once PIN was entered and authentication has commenced
             Thread thread = new Thread(() -> {
                 String pass = sharedPref.getString("password", "0");
                 String content = PIN.getText().toString();
-                int pin = Integer.parseInt(content.equals("") ? "0" : content);
-                JSONObject data = Net.createJSON(new Pair<>("pass", pass), new Pair<>("PIN", pin));
+                int pin = Integer.parseInt(content.equals("") ? "0" : content); // protection from parsing error for empty value in PIN
+                JSONObject data = Net.createJSON(new Pair<>("pass", pass), new Pair<>("PIN", pin)); // generate JSON with password and PIN for auth
                 Net.Method method = Net.Method.GET;
                 String destURL = "http://192.168.111.125:5000/?"; // TODO: url is currently dynamic, need to convert to some sort of DNS perhaps
                 String responseData = "Request Failed";
                 try {
-                    responseData = Net.request(destURL, method, data);
+                    responseData = Net.request(destURL, method, data); // send authentication request
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
                 System.out.println(responseData);
-                activity = responseData;
+                activity = responseData; // temporary hadling with response before embedding the response data in system
             });
 
             thread.start();
             long tStart = System.currentTimeMillis();
-            while (activity.equals("") && System.currentTimeMillis() - tStart < 5000) {
+            while (activity.equals("") && System.currentTimeMillis() - tStart < 5000) { // wait for response with timeout of 5 seconds to get data.
                 System.out.println("waiting");
             }
 
