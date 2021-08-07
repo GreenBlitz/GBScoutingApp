2532997dff8f7e4c43e567372f281c2c461ea24a
TheOnion313 <royn77777@gmail.com>
2021-08-07 22:15:28 +0300

    Roy - Started implementing ScoutingEntry which represents the data sent from a scouting session of a game on a robot, created constructor but had to wrap up for the day so not done.

--- app/src/test/java/scoutingtest4590/util/scouter/ScoutingEntry.java
+++ app/src/test/java/scoutingtest4590/util/scouter/ScoutingEntry.java
@@ -1,4 +1,19 @@
 package scoutingtest4590.util.scouter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class ScoutingEntry {
+    private ArrayList<ScoutingData<Object>> gameEntry;
+
+    public ScoutingEntry(ArrayList<ScoutingData<Object>> gameEntry) {
+        this.gameEntry = gameEntry;
+    }
+
+    public ScoutingEntry(ScoutingData<Object>... gameEntry) {
+        ArrayList<ScoutingData<Object>> a = new ArrayList<>();
+        Collections.addAll(a, gameEntry);
+        this.gameEntry = a;
+    }
 }
