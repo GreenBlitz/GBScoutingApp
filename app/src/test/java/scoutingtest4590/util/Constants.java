5ea5bbb54816d4bf97203a21a75019c15a883f1c
TheOnion313 <royn77777@gmail.com>
2021-08-14 17:31:52 +0300

    Roy - created Constants class which will hold all relevant application constants (kind of like RobotMap) such as the server IP etc.

---
+++ app/src/test/java/scoutingtest4590/util/Constants.java
@@ -0,0 +1,8 @@
+package scoutingtest4590.util;
+
+public static class Constants {
+
+    public static class Networking {
+        public static final String serverURL = "http://downloadmoreram.com:5000";
+    }
+}
