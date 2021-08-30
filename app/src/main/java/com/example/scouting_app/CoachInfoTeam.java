7fa97b78df5aa58d555149c90bfae1398b7d5628
TheOnion313 <royn77777@gmail.com>
2021-08-30 16:29:26 +0300

    Roy - refactored and tidied up some GUI

--- app/src/main/java/com/example/scouting_app/CoachInfoTeam.java
+++ app/src/main/java/com/example/scouting_app/CoachInfoTeam.java
@@ -27,5 +27,45 @@ public class CoachInfoTeam extends AppCompatActivity {
 
         TextView ranking = (TextView) findViewById(R.id.ranking);
         ranking.setText("2");
+
+        TextView avgAuto = (TextView) findViewById(R.id.avgAuto);
+        avgAuto.setText("9000");
+
+        TextView maxAuto = (TextView) findViewById(R.id.maxAuto);
+        maxAuto.setText("9000");
+
+        TextView avgTele = (TextView) findViewById(R.id.avgTele);
+        avgTele.setText("9000");
+
+        TextView maxTele = (TextView) findViewById(R.id.maxTele);
+        maxTele.setText("9000");
+
+        TextView avgCycles = (TextView) findViewById(R.id.avgCycle);
+        avgCycles.setText("9000");
+
+        TextView maxCycles = (TextView) findViewById(R.id.maxCycle);
+        maxCycles.setText("9000");
+
+        TextView percentClimb = (TextView) findViewById(R.id.percentClimbing);
+        percentClimb.setText("9000");
+
+        TextView lastClimb = (TextView) findViewById(R.id.lastClimbing);
+        lastClimb.setText("9000");
+
+        TextView percentWheelCount = (TextView) findViewById(R.id.percentWheelCount);
+        percentWheelCount.setText("9000");
+
+        TextView lastWheelCount = (TextView) findViewById(R.id.lastWheelCount);
+        lastWheelCount.setText("9000");
+
+        TextView percentWheelColor = (TextView) findViewById(R.id.percentWheelColor);
+        percentWheelColor.setText("9000");
+
+        TextView lastWheelColor = (TextView) findViewById(R.id.lastWheelColor);
+        lastWheelColor.setText("9000");
+
+        TextView comments = (TextView) findViewById(R.id.comments);
+        comments.setText("over 9 thousand");
+
     }
 }
