48e73cb895e56970b729d428f7ba51d59ccee842
TheOnion313 <royn77777@gmail.com>
2021-09-16 15:35:50 +0300

    Roy - added communications problem activity

---
+++ app/src/main/java/com/example/scouting_app/NetworkProblemsPage.java
@@ -0,0 +1,14 @@
+package com.example.scouting_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NetworkProblemsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_network_problems_page);
+    }
+}
